d249dd58d396d5b0c9a821b392c5a96fb889339b
ChestonC <cchen208@ucsc.edu>
2021-05-14 20:49:11 -0700

    Create Princess.js

---
+++ src/prefabs/Princess.js
@@ -0,0 +1,15 @@
+class Princess extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+
+
+    }
+    preload() {
+        this.load.image('princess', './assets/testprincess.png');
+    }
+    update() {
+
+    }
+
+}
